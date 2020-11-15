b51ca8336ede19838bdb708bf19b93137b2b7afe
Srivardhan Sajja <sajja3@illinois.edu>
2020-11-15 16:41:26 -0600

    created space for results

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', result=False)
+
+@app.route('/model', methods=['POST'])
+def model():
+    return render_template('index.html', result=True, value='Hello')
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
