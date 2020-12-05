8d8af1e1af4c91449ab1e055c9c627903371d5d0
navyaasanan <navyaas2@illinois.edu>
2020-12-05 12:00:32 -0600

    Integration

--- model_deploy/model_dep.py
+++ model_deploy/model_dep.py
@@ -1,27 +1,58 @@
 from types import ClassMethodDescriptorType
+import numpy as np
+import json
+from collections import Counter
 
 
 def read_physical_model():
-    f = open("model", "r")
-    pass
+    with open("model_deploy/model.json", "r") as fp:
+        data = json.load(fp)
+    positive_words = Counter(data["positives"])
+    mean_len = data["mean_len"]
+    std_dev = data["std_dev"]
+    return positive_words, mean_len, std_dev
 
 
-def classify(model, dev_set):
-    pass
+
+def classify(postive_words, dev_set, mean_len, std_dev):
+
+    predicted_labels = np.zeros(len(dev_set))
+
+    trim = {'/', '?', '.php','.html','.shtml'}
+
+    for i in range(len(dev_set)):
+
+        for elem in trim:
+            if dev_set[i].endswith(elem):
+                dev_set[i] = dev_set[i][:-len(elem)]
+     
+        for j in range(6):
+            if (dev_set[i].endswith(postive_words.most_common(6)[j][0])):
+                predicted_labels[i] = 1
+
+        for j in range(7, 10):
+            if ((postive_words.most_common(11)[j][0]) in dev_set[i]):
+                predicted_labels[i] = 1
+
+        if (np.absolute(len(dev_set[i]) - mean_len) <  std_dev/25):
+            predicted_labels[i] = 1
+    
+
+    return predicted_labels 
 
 
 def model_deploy(unfiltered_urls):
 
-    # model = read_physical_model()
+    model, mean_len, std_dev = read_physical_model()
 
-    # labels = classify(model, unfiltered_urls)
+    labels = classify(model, unfiltered_urls, mean_len, std_dev)
 
-    # filtered_list = list()
-    # for i in range(len(unfiltered_urls)):
-    #     if labels[i] == 1:
-    #         filtered_list.append(unfiltered_urls[i])
+    filtered_list = list()
+    for i in range(len(unfiltered_urls)):
+        if labels[i] == 1:
+            filtered_list.append(unfiltered_urls[i])
 
     # apply model, and filter out unwanted urls
-    filtered_list = unfiltered_urls  # comment out line
+    #filtered_list = unfiltered_urls  # comment out line
 
     return filtered_list
